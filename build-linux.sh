bd5ed96e3d26063f5597cfbd99309eadf1f97d37
Andrea Medeghini <andrea@nextbreakpoint.com>
2016-07-22 18:50:49 +0100

    Added script for linux and macos

---
+++ build-linux.sh
@@ -0,0 +1,4 @@
+#/bin/sh
+cd FreeImage
+make -f Makefile.lib.linux all
+make -f Makefile.jni.linux all
---
+++ build-macos.sh
@@ -0,0 +1,4 @@
+#/bin/sh
+cd FreeImage
+make -f Makefile.lib.macos all
+make -f Makefile.jni.macos all
