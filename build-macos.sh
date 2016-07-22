ed6556b40059593f08b1294e5d5ce5db0b4aafe8
Andrea Medeghini <andrea@nextbreakpoint.com>
2016-07-23 00:04:18 +0100

    Added scripts for macos

--- build-macos.sh
+++ build-macos.sh
@@ -1,4 +1,15 @@
 #/bin/sh
+rm -fR FreeImage
+
+cvs -z3 -d:pserver:anonymous@freeimage.cvs.sourceforge.net:/cvsroot/freeimage co -P FreeImage
+
+cp Makefile.lib.macos FreeImage
+cp Makefile.jni.macos FreeImage
+cp freeimage4java.i FreeImage
+
+patch -p0 < macos.patch
+
 cd FreeImage
+
 make -f Makefile.lib.macos all
 make -f Makefile.jni.macos all
