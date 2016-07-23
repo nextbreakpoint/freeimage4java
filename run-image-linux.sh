e8001bba87713e61c0d08d72a87b6a6a4b066a37
Andrea Medeghini <andrea@nextbreakpoint.com>
2016-07-23 15:41:35 +0100

    Fixed volume names

--- run-image-linux.sh
+++ run-image-linux.sh
@@ -1,4 +1,3 @@
-mkdir -p build/linux
 mkdir -p dist/linux
 
-docker run --name freeimage4java-linux --rm -i --volume=build/linux:/build --volume=dist/linux:/dist freeimage4java-linux
+docker run --name freeimage4java-linux --rm -i --volume=dist:/dist freeimage4java-linux
--- run-image-mingw64.sh
+++ run-image-mingw64.sh
@@ -1,4 +1,3 @@
-mkdir -p build/win32
 mkdir -p dist/win32
 
-docker run --name freeimage4java-mingw64 --rm -i --volume=build/win32:/build --volume=dist/win32:/dist freeimage4java-mingw64
+docker run --name freeimage4java-mingw64 --rm -i -volume=dist:/dist freeimage4java-mingw64
