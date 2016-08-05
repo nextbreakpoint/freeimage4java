ba78e30dbc377274f38f197311733b781d62b757
Andrea Medeghini <andrea@nextbreakpoint.com>
2016-08-05 09:16:39 +0100

    Renamed patches

--- build-macos.sh
+++ build-macos.sh
@@ -6,12 +6,10 @@ rm -fR FreeImage
 cvs -z3 -d:pserver:anonymous@freeimage.cvs.sourceforge.net:/cvsroot/freeimage co -P FreeImage
 
 cp Makefile.lib.macos FreeImage/Makefile.lib.macos
-
 cp Makefile.jni.macos FreeImage/Makefile.jni.macos
 
-cp macos.patch FreeImage/macos.patch
-
-patch -p0 < FreeImage/macos.patch
+cp Patch.macos FreeImage/Patch.macos
+patch -p0 < FreeImage/Patch.macos
 
 cd FreeImage
 
